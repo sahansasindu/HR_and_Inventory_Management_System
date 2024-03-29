d4916d21dbc96657cfb41db26ac3ad08efcd5c26
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-03-30 01:17:59 +0530

    created wash  bottle section

--- src/app/store_keeper/sections/emptysection/emptysection.component.ts
+++ src/app/store_keeper/sections/emptysection/emptysection.component.ts
@@ -6,37 +6,22 @@ import {MatTableDataSource} from "@angular/material/table";
   templateUrl: './emptysection.component.html',
   styleUrl: './emptysection.component.css'
 })
-export class EmptysectionComponent {
-  displayedColumns: string[] = ['empty_unit_id', 'damage_bottles', 'empty_bottles', 'for_washing', 'submit_date', 'submit_time'];
+export class EmptysectionComponent implements OnInit{
+  displayedColumns: string[] = ['empty_unit_id', 'empty_bottles','damage_bottles', 'for_washing', 'submit_date', 'submit_time'];
   dataSource = new MatTableDataSource<TableElement>([]);
   selectedRow: Element | null = null;
   // You will replace this with your actual data
   ELEMENT_DATA: TableElement[] = [
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
-    { empty_unit_id: 1, damage_bottles: 5, empty_bottles: 10, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+    { empty_unit_id: 1, empty_bottles: 10,damage_bottles: 5, for_washing: 15, submit_date: new Date(), submit_time: new Date() },
+
     // ... more data
   ];
 
@@ -47,6 +32,28 @@ export class EmptysectionComponent {
   selectRow(row: Element): void {
     this.selectedRow = row;
   }
+
+  isAddDetailsVisible: boolean = false; // Used to toggle the add details view
+  isUpdateVisible: boolean = false; // Used to toggle the update view
+
+  //the method use for visible to the add details form
+  toggleAddDetails(): void {
+    this.isAddDetailsVisible = !this.isAddDetailsVisible;
+    // Ensure update div is closed when opening add details
+    if (this.isAddDetailsVisible) {
+      this.isUpdateVisible = false;
+    }
+  }
+
+  toggleUpdate(): void {
+    this.isUpdateVisible = !this.isUpdateVisible;
+    // Ensure add details div is closed when opening update
+    if (this.isUpdateVisible) {
+      this.isAddDetailsVisible = false;
+    }
+  }
+
+
 }
 
 export interface TableElement {
--- src/app/store_keeper/sections/washingsection/washingsection.component.ts
+++ src/app/store_keeper/sections/washingsection/washingsection.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-washingsection',
   templateUrl: './washingsection.component.html',
   styleUrl: './washingsection.component.css'
 })
-export class WashingsectionComponent {
+export class WashingsectionComponent implements OnInit{
+  displayedColumns: string[] = ['employeeid', 'employeename','damage_bottles','submit_date'];
+  dataSource = new MatTableDataSource<TableElement>([]);
+  selectedRow: Element | null = null;
+  // You will replace this with your actual data
+  ELEMENT_DATA: TableElement[] = [
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
+    { employeeid: "EPF00001", employeename: "Kamal",damage_bottles: 5, submit_date: new Date() },
 
+
+    // ... more data
+  ];
+
+  ngOnInit() {
+    this.dataSource.data = this.ELEMENT_DATA;
+  }
+
+  selectRow(row: Element): void {
+    this.selectedRow = row;
+  }
+
+  isAddDetailsVisible: boolean = false; // Used to toggle the add details view
+  isUpdateVisible: boolean = false; // Used to toggle the update view
+
+  //the method use for visible to the add details form
+  toggleAddDetails(): void {
+    this.isAddDetailsVisible = !this.isAddDetailsVisible;
+    // Ensure update div is closed when opening add details
+    if (this.isAddDetailsVisible) {
+      this.isUpdateVisible = false;
+    }
+  }
+
+  toggleUpdate(): void {
+    this.isUpdateVisible = !this.isUpdateVisible;
+    // Ensure add details div is closed when opening update
+    if (this.isUpdateVisible) {
+      this.isAddDetailsVisible = false;
+    }
+  }
+
+
+}
+
+export interface TableElement {
+  employeeid: String;
+  employeename: String;
+  damage_bottles: number;
+  submit_date: Date;
 }
