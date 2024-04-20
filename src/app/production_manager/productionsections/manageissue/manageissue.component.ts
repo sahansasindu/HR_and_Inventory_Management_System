8e23732acd4ab6494756a2bfa03dba8e046fb7c8
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-04-21 01:32:44 +0530

    create front end Issue section and bottle Stock

--- src/app/production_manager/productionsections/manageissue/manageissue.component.ts
+++ src/app/production_manager/productionsections/manageissue/manageissue.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {Agent} from "../../../model/agentmodel";
+import {ProductionIssue} from "../../../model/issuemodel";
 
 @Component({
   selector: 'app-manageissue',
@@ -10,6 +13,60 @@ export class ManageissueComponent implements data{
 
   chart: any;
   isButtonVisible = false;
+  isManageProductionIssues: boolean=false;
+  isaddNewIssue: boolean=false;
+  isupdateIssue: boolean=false;
+  isdeleteIssue: boolean=false;
+
+
+  displayedColumns: string[] = ['issue_id', 'issue_name'];
+  dataSourceIssue = new MatTableDataSource<ProductionIssue>([]);
+  selectedRow: ProductionIssue | null = null;
+
+
+  selectRow(row: ProductionIssue): void {
+    this.selectedRow = row;
+    console.log(this.selectedRow);
+
+  }
+
+  manageIssue() {
+
+    this.isManageProductionIssues=!this.isManageProductionIssues;
+
+  }
+
+  visibleaddIssue() {
+
+    this.isaddNewIssue=!this.isaddNewIssue;
+
+    if(this.isaddNewIssue){
+      this.isupdateIssue=false;
+      this.isdeleteIssue=false;
+    }
+  }
+
+  visibleupdateIssue() {
+
+    this.isupdateIssue=!this.isupdateIssue;
+
+    if(this.isupdateIssue){
+      this.isaddNewIssue=false;
+      this.isdeleteIssue=false;
+    }
+
+  }
+
+  visibleDeleteIssue() {
+
+    this.isdeleteIssue=!this.isdeleteIssue;
+
+    if(this.isdeleteIssue){
+      this.isupdateIssue=false;
+      this.isaddNewIssue=false;
+    }
+
+  }
 
   visitorsChartDrilldownHandler = (e: any) => {
     this.chart.options = this.visitorsDrilldownedChartOptions;
@@ -109,6 +166,9 @@ export class ManageissueComponent implements data{
       ]
     }]
   };
+
+
+
 
   handleClick(event: Event) {
     this.chart.options = this.newVSReturningVisitorsOptions;
