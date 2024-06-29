8d560858e21df30466ba0ab5bee3e45090ec243a
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-06-30 02:02:55 +0530

    hr manager dashboard

--- src/app/hr_manager/hrm-dashboard/hrm-dashboard.component.ts
+++ src/app/hr_manager/hrm-dashboard/hrm-dashboard.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hrm-dashboard',
   templateUrl: './hrm-dashboard.component.html',
   styleUrl: './hrm-dashboard.component.css'
 })
-export class HrmDashboardComponent {
+export class HrmDashboardComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  cards = [
+    { title: 'Company Total Employees', value: 200 },
+    { title: 'Currently working Employees', value: 183 },
+    { title: 'Today Absent Employees', value: 10 },
+    { title: 'Today Short Leavers', value: 7 },
+    { title: 'Currently Gate pass', value: 5 }
+  ];
 
 }
