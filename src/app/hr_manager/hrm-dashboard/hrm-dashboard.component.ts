68f3988813d3399e8d7b74b92331bdb70f92f7c3
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-06-30 15:02:46 +0530

    Design hr manager employee chart and department chart front end

--- src/app/hr_manager/hrm-dashboard/hrm-dashboard.component.ts
+++ src/app/hr_manager/hrm-dashboard/hrm-dashboard.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HrmDashboardComponent implements OnInit {
 
+  selectedChart: string = 'departments';
+
+
   constructor() {}
 
   ngOnInit() {
@@ -16,8 +19,110 @@ export class HrmDashboardComponent implements OnInit {
     { title: 'Company Total Employees', value: 200 },
     { title: 'Currently working Employees', value: 183 },
     { title: 'Today Absent Employees', value: 10 },
-    { title: 'Today Short Leavers', value: 7 },
     { title: 'Currently Gate pass', value: 5 }
   ];
+
+  chartOptionsDep = {
+    animationEnabled: true,
+    title: {
+      text: "Company Departments"
+    },
+    legend: {
+      verticalAlign: "bottom",
+      horizontalAlign: "center",
+      fontSize: 13,
+      fontFamily: "Helvetica",
+      fontColor: "#000000",
+      cursor: "pointer",
+      itemTextFormatter: function(e: any) {
+        return `${e.dataPoint.name}: ${e.dataPoint.y}`;
+      },
+      itemWrap: true
+    },
+    data: [{
+      type: "pie",
+      startAngle: -90,
+      indexLabel: "{name}: {y}",
+      yValueFormatString: "#,###",
+      showInLegend: true,
+      dataPoints: [
+        { y: 34, name: "Finance" },
+        { y: 23, name: "Human Resource" },
+        { y: 75, name: "Marketing" },
+        { y: 240, name: "Production" },
+        { y: 80, name: "Operating" },
+        { y: 35, name: "Sales" }
+      ]
+    }]
+  };
+
+  chartOptionsEmp = {
+    animationEnabled: true,
+    title: {
+      text: "Employee Status"
+    },
+    axisX: {
+      labelAngle: 0
+    },
+    axisY: {
+      title: "Number Of Male Employees"
+    },
+    axisY2: {
+      title: "Number Of Female Employees"
+    },
+    toolTip: {
+      shared: true,
+    },
+    legend:{
+      cursor:"pointer",
+      itemclick: function(e: any){
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+          e.dataSeries.visible = false;
+        }
+        else {
+          e.dataSeries.visible = true;
+        }
+        e.chart.render();
+      }
+    },
+    data: [{
+      type: "column",
+      name: "Total male Employees",
+      legendText: "Male",
+      showInLegend: true,
+      color: "#ec4040",
+      dataPoints:[
+        {label: "Finance", y: 262},
+        {label: "Human Resource", y: 211},
+        {label: "Marketing", y: 175},
+        {label: "Production", y: 137},
+        {label: "Operating", y: 115},
+        {label: "Sales", y: 104}
+      ]
+    }, {
+      type: "column",
+      name: "Total Female Employees",
+      legendText: "Female",
+      axisYType: "secondary",
+      showInLegend: true,
+      color: "#003cff",
+      dataPoints:[
+        {label: "Finance", y: 100},
+        {label: "Human Resource", y: 150},
+        {label: "Marketing", y: 75},
+        {label: "Production", y: 80},
+        {label: "Operating", y: 90},
+        {label: "Sales", y: 104}
+      ]
+    }]
+  }
+
+  showDepartmentsChart() {
+    this.selectedChart = 'departments';
+  }
+
+  showEmployeesChart() {
+    this.selectedChart = 'employees';
+  }
 
 }
